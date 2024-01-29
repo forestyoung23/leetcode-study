d0e2b8d7274cb2f63f51066f2d3fa9e9d90d1a42
Forest <forestyoung23@gmail.com>
2024-01-29 19:15:39 +0800

    插入排序

---
+++ src/main/java/com/forest/leetcode/other/sort/InsertionSort.java
@@ -0,0 +1,18 @@
+package com.forest.leetcode.other.sort;
+
+/**
+ * 插入排序
+ *
+ * @author Forest Dong
+ * @date 2024年01月29日 19:08
+ */
+public class InsertionSort {
+
+    private static void sort(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+
+            }
+        }
+    }
+}
