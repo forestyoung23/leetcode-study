a8a293d89c773010af917024f579db9a48885cfe
Forest <forestyoung23@gmail.com>
2024-01-30 19:50:12 +0800

    力扣922

--- src/main/java/com/forest/leetcode/day12/Solution922.java
+++ src/main/java/com/forest/leetcode/day12/Solution922.java
@@ -8,6 +8,14 @@ package com.forest.leetcode.day12;
  * @date 2024年01月30日 17:49
  */
 public class Solution922 {
+    /**
+     * 定义两个指针i和j，分别记录偶数和奇数对应位置的元素，每次各加2，并把奇偶排序后的结果存在新数组中。
+     *
+     * @param nums
+     * @return {@link int[] }
+     * @author Forest Dong
+     * @date 2024/01/30 19:49
+     */
     public static int[] sortArrayByParityII(int[] nums) {
         int[] result = new int[nums.length];
         int i = 0, j = 1;
@@ -24,7 +32,7 @@ public class Solution922 {
     }
 
     public static void main(String[] args) {
-        int[] num = {4,2,5,7};
+        int[] num = {4, 2, 5, 7};
         int[] array = sortArrayByParityII(num);
         for (int i : array) {
             System.err.println(i);
