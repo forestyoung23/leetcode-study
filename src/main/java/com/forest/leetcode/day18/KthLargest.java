f52712263440b9fc4275fbec5d6840b1171141eb
Forest <forestyoung23@gmail.com>
2024-02-19 18:00:16 +0800

    力扣703

---
+++ src/main/java/com/forest/leetcode/day18/KthLargest.java
@@ -0,0 +1,20 @@
+package com.forest.leetcode.day18;
+
+/**
+ * @author Forest Dong
+ * @date 2024年02月19日 17:54
+ */
+public class KthLargest {
+
+    private int[] nums;
+
+    private int k;
+    public KthLargest(int k, int[] nums) {
+        this.nums = nums;
+        this.k = k;
+    }
+
+    public int add(int val) {
+
+    }
+}
---
+++ src/main/java/com/forest/leetcode/day18/Solution703.java
@@ -0,0 +1,12 @@
+package com.forest.leetcode.day18;
+
+/**
+ * 力扣题目序号：703.数据流中的第 K 大元素
+ * 难度：简单
+ *
+ * @author Forest Dong
+ * @link https://leetcode.cn/problems/kth-largest-element-in-a-stream/description/
+ * @date 2024年02月19日 17:53
+ */
+public class Solution703 {
+}
