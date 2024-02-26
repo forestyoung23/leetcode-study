bd33f26243feac98ad0da31b033cc334a88fee34
Forest <forestyoung23@gmail.com>
2024-02-26 17:30:03 +0800

    力扣 409

---
+++ src/main/java/com/forest/leetcode/day20/Solution409.java
@@ -0,0 +1,12 @@
+package com.forest.leetcode.day20;
+
+/**
+ * 力扣题目序号：1822.数组元素积的符号
+ * 难度：简单
+ *
+ * @author Forest Dong
+ * @link https://leetcode.cn/problems/sign-of-the-product-of-an-array/description/
+ * @date 2024年02月26日 16:34
+ */
+public class Solution409 {
+}
