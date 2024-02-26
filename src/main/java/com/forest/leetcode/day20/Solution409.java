69ebe910cb918678a8eaf5274c8d0de5e8d6fcbe
Forest <forestyoung23@gmail.com>
2024-02-26 17:30:49 +0800

    力扣 409

--- src/main/java/com/forest/leetcode/day20/Solution409.java
+++ src/main/java/com/forest/leetcode/day20/Solution409.java
@@ -1,11 +1,11 @@
 package com.forest.leetcode.day20;
 
 /**
- * 力扣题目序号：1822.数组元素积的符号
+ * 力扣题目序号：409.最长回文串
  * 难度：简单
  *
  * @author Forest Dong
- * @link https://leetcode.cn/problems/sign-of-the-product-of-an-array/description/
+ * @link https://leetcode.cn/problems/longest-palindrome/description/
  * @date 2024年02月26日 16:34
  */
 public class Solution409 {
