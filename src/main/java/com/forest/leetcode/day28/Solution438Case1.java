1ac94b42886ce72bd82471dd4d8212fbd56e7851
Forest <forestyoung23@gmail.com>
2024-03-08 17:31:49 +0800

    力扣438

---
+++ src/main/java/com/forest/leetcode/day28/Solution438Case1.java
@@ -0,0 +1,53 @@
+package com.forest.leetcode.day28;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 力扣题目序号：438.找到字符串中所有字母异位词
+ * 难度：中等
+ *
+ * @author Forest Dong
+ * @link https://leetcode.cn/problems/find-all-anagrams-in-a-string/description/?envType=study-plan-v2&envId=top-100-liked
+ * @date 2024年03月08日 16:42
+ */
+public class Solution438Case1 {
+    public static List<Integer> findAnagrams(String s, String p) {
+        int sLen = s.length(), pLen = p.length();
+
+        if (sLen < pLen) {
+            return new ArrayList<Integer>();
+        }
+
+        List<Integer> ans = new ArrayList<Integer>();
+        int[] sCount = new int[26];
+        int[] pCount = new int[26];
+        for (int i = 0; i < pLen; ++i) {
+            ++sCount[s.charAt(i) - 'a'];
+            ++pCount[p.charAt(i) - 'a'];
+        }
+
+        if (Arrays.equals(sCount, pCount)) {
+            ans.add(0);
+        }
+
+        for (int i = 0; i < sLen - pLen; ++i) {
+            --sCount[s.charAt(i) - 'a'];
+            ++sCount[s.charAt(i + pLen) - 'a'];
+
+            if (Arrays.equals(sCount, pCount)) {
+                ans.add(i + 1);
+            }
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        String s = "cbaebabacd";
+        String p = "abc";
+        List<Integer> anagrams = findAnagrams(s, p);
+        System.err.println(anagrams);
+    }
+}
