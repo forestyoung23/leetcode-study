8647042e04569749d336e7f5bc89c85320990cf8
Forest <forestyoung23@gmail.com>
2024-04-25 19:18:02 +0800

    力扣199

---
+++ src/main/java/com/forest/leetcode/day63/Solution199Case1.java
@@ -0,0 +1,52 @@
+package com.forest.leetcode.day63;
+
+import com.forest.leetcode.common.TreeNode;
+
+import java.util.*;
+
+/**
+ * 力扣题目序号：199.二叉树的右视图
+ * 难度：中等
+ *
+ * @author Forest Dong
+ * @link https://leetcode.cn/problems/binary-tree-right-side-view/description/?envType=study-plan-v2&envId=top-100-liked
+ * @date 2024年04月25日 19:07
+ */
+public class Solution199Case1 {
+    public List<Integer> rightSideView(TreeNode root) {
+        Map<Integer, Integer> rightmostValueAtDepth = new HashMap<>();
+        int max_depth = -1;
+
+        Deque<TreeNode> nodeStack = new LinkedList<>();
+        Deque<Integer> depthStack = new LinkedList<>();
+        nodeStack.push(root);
+        depthStack.push(0);
+
+        while (!nodeStack.isEmpty()) {
+            TreeNode node = nodeStack.pop();
+            int depth = depthStack.pop();
+
+            if (node != null) {
+                // 维护二叉树的最大深度
+                max_depth = Math.max(max_depth, depth);
+
+                // 如果不存在对应深度的节点我们才插入
+                if (!rightmostValueAtDepth.containsKey(depth)) {
+                    rightmostValueAtDepth.put(depth, node.val);
+                }
+
+                nodeStack.push(node.left);
+                nodeStack.push(node.right);
+                depthStack.push(depth + 1);
+                depthStack.push(depth + 1);
+            }
+        }
+
+        List<Integer> rightView = new ArrayList<Integer>();
+        for (int depth = 0; depth <= max_depth; depth++) {
+            rightView.add(rightmostValueAtDepth.get(depth));
+        }
+
+        return rightView;
+    }
+}
