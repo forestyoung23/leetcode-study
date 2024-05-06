1e01759f4c1be85cfcba8024e234e538dbfd6e72
Forest <forestyoung23@gmail.com>
2024-05-06 19:20:50 +0800

    力扣105

---
+++ src/main/java/com/forest/leetcode/day65/Solution105Case1.java
@@ -0,0 +1,59 @@
+package com.forest.leetcode.day65;
+
+import com.forest.leetcode.common.TreeNode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 力扣题目序号：105.从前序与中序遍历序列构造二叉树
+ * 难度：中等
+ *
+ * @author Forest Dong
+ * @link https://leetcode.cn/problems/construct-binary-tree-from-preorder-and-inorder-traversal/description/?envType=study-plan-v2&envId=top-100-liked
+ * @date 2024年05月06日 19:11
+ */
+public class Solution105Case1 {
+
+    private Map<Integer, Integer> indexMap;
+    public TreeNode myBuildTree(int[] preorder, int[] inorder, int preorder_left, int preorder_right, int inorder_left, int inorder_right) {
+        if (preorder_left > preorder_right) {
+            return null;
+        }
+
+        // 前序遍历中的第一个节点就是根节点
+        int preorder_root = preorder_left;
+        // 在中序遍历中定位根节点
+        int inorder_root = indexMap.get(preorder[preorder_root]);
+
+        // 先把根节点建立出来
+        TreeNode root = new TreeNode(preorder[preorder_root]);
+        // 得到左子树中的节点数目
+        int size_left_subtree = inorder_root - inorder_left;
+        // 递归地构造左子树，并连接到根节点
+        // 先序遍历中「从 左边界+1 开始的 size_left_subtree」个元素就对应了中序遍历中「从 左边界 开始到 根节点定位-1」的元素
+        root.left = myBuildTree(preorder, inorder, preorder_left + 1, preorder_left + size_left_subtree, inorder_left, inorder_root - 1);
+        // 递归地构造右子树，并连接到根节点
+        // 先序遍历中「从 左边界+1+左子树节点数目 开始到 右边界」的元素就对应了中序遍历中「从 根节点定位+1 到 右边界」的元素
+        root.right = myBuildTree(preorder, inorder, preorder_left + size_left_subtree + 1, preorder_right, inorder_root + 1, inorder_right);
+        return root;
+    }
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        int n = preorder.length;
+        // 构造哈希映射，帮助我们快速定位根节点
+        indexMap = new HashMap<Integer, Integer>();
+        for (int i = 0; i < n; i++) {
+            indexMap.put(inorder[i], i);
+        }
+        return myBuildTree(preorder, inorder, 0, n - 1, 0, n - 1);
+    }
+
+    public static void main(String[] args) {
+        int[] preorder = {3, 9, 20, 15, 7};
+        int[] inorder = {9, 3, 15, 20, 7};
+        Solution105Case1 solution = new Solution105Case1();
+        TreeNode treeNode = solution.buildTree(preorder, inorder);
+        System.out.println(treeNode);
+    }
+}
