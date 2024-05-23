4b1f5ab858089002cd2d4eccf9e2cd8d6779dfd0
Forest <forestyoung23@gmail.com>
2024-05-23 20:15:38 +0800

    力扣20

---
+++ src/main/java/com/forest/leetcode/day70/Solution20Case1.java
@@ -0,0 +1,47 @@
+package com.forest.leetcode.day70;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * 力扣题目序号：20.有效的括号
+ * 难度：简单
+ *
+ * @author Forest Dong
+ * @link https://leetcode.cn/problems/valid-parentheses/description/?envType=study-plan-v2&envId=top-100-liked
+ * @date 2024年05月23日 19:52
+ */
+public class Solution20Case1 {
+    private static final List<Character> leftList = List.of('(', '[', '{');
+    private static final List<Character> rightList = List.of(')', ']', '}');
+
+    public boolean isValid(String s) {
+        char[] chars = s.toCharArray();
+        if (null == chars || 0 == chars.length) {
+            return false;
+        }
+        if (rightList.contains(chars[0])) {
+            return false;
+        }
+        Stack<Character> stack = new Stack<>();
+        for (char aChar : chars) {
+            if (leftList.contains(aChar)) {
+                stack.add(aChar);
+            }
+            if (stack.isEmpty()) {
+                return false;
+            }
+            if (rightList.contains(aChar)) {
+                Character peek = stack.peek();
+                if (('(' == peek && ')' == aChar) || ('[' == peek && ']' == aChar)  || ('{' == peek && '}' == aChar) ) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
---
+++ src/main/java/com/forest/leetcode/day70/Solution20Case2.java
@@ -0,0 +1,30 @@
+package com.forest.leetcode.day70;
+
+import java.util.*;
+
+/**
+ * 力扣题目序号：20.有效的括号
+ * 难度：简单
+ *
+ * @author Forest Dong
+ * @link https://leetcode.cn/problems/valid-parentheses/description/?envType=study-plan-v2&envId=top-100-liked
+ * @date 2024年05月23日 20:12
+ */
+public class Solution20Case2 {
+    public boolean isValid(String s) {
+        char[] chs = s.toCharArray();
+        Stack<Character> stack = new Stack<>();
+        for (char ch : chs) {
+            if (ch == '(' || ch == '[' || ch == '{'){
+                stack.push(ch);
+            }else{
+                if (stack.isEmpty())    return false;
+                Character target = stack.pop();
+                if ((ch == ')' && target != '(') || (ch == ']' && target != '[') || (ch == '}' && target != '{')){
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
