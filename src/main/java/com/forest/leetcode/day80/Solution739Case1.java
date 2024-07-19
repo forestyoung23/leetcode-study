910050b0879b14c442798a966a066cafdfc688a1
Forest <forestyoung23@gmail.com>
2024-07-19 17:59:53 +0800

    力扣739

---
+++ src/main/java/com/forest/leetcode/day80/Solution739Case1.java
@@ -0,0 +1,17 @@
+package com.forest.leetcode.day80;
+
+/**
+ * 力扣题目序号：739.每日温度
+ * 难度：中等
+ *
+ * @author Forest Dong
+ * @link https://leetcode.cn/problems/daily-temperatures/description/?envType=study-plan-v2&envId=top-100-liked
+ * @date 2024年07月19日 17:24
+ */
+public class Solution739Case1 {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] result = new int[temperatures.length];
+        for (int i = 0; i < temperatures.length; i++) {}
+        return result;
+    }
+}
